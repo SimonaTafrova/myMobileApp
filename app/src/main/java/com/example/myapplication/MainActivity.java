af4acba73e78f03c7957719bbe5d5ed48420d57a
Simona Tafrova <simonatafrova@gmail.com>
2024-04-24 15:05:55 +0300

    Created RegisterActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void disable(View view){
-        view.setEnabled(false);
+    public void register(View v){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
+
+
 }
