328f8ba3027b8fa32bb419899b9e51e6b19f12b7
Simona Tafrova <simonatafrova@gmail.com>
2024-05-09 18:13:08 +0300

    Removed a method

--- app/src/main/java/com/example/myapplication/PlayActivity.java
+++ app/src/main/java/com/example/myapplication/PlayActivity.java
@@ -44,7 +44,7 @@ public class PlayActivity extends AppCompatActivity {
             counterO++;
         }
 
-        button.setTextSize(50);
+        button.setTextSize(60);
         button.setEnabled(false);
 
 
